f5469b4a5aae34eed0bf14deb769e1f3c8aed990
ubuntu-pigkun <zk159311@163.com>
2025-04-07 02:33:55 -0700

    网络模块解耦完成，初步测试代码无误

--- src/server/chatserver.cpp
+++ src/server/chatserver.cpp
@@ -1,8 +1,12 @@
 #include "chatserver.hpp"
+#include "json.hpp"
+#include "chatservice.hpp"
 
 #include <functional>
+#include <string>
 using namespace std;
 using namespace placeholders;
+using json = nlohmann::json;
 
 ChatServer::ChatServer(EventLoop *loop,
          const InetAddress &listenAddr,
@@ -27,13 +31,23 @@ void ChatServer::start(){
 
 // 上报连接相关的回调函数
 void ChatServer::onConnection(const TcpConnectionPtr &conn){
-
+    // 客户断开连接
+    if(!conn->connected()){
+        conn->shutdown();
+    }
 }
 
 // 上报读写事件相关的回调函数
 void ChatServer::onMessage(const TcpConnectionPtr &conn,
-                   Buffer *buf,
+                   Buffer *buffer,
                    Timestamp time)
 {
-    
+    string buf = buffer->retrieveAllAsString();
+    // 数据的反序列化
+    json js = json::parse(buf);
+    // 达到的目的：完全解耦网络模块的代码和业务模块的代码
+    // 通过js["msgid"] 获取=>业务hander=>conn js time...
+    auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
+    // 回调消息绑定好的事件处理器，来执行相应的业务处理
+    msgHandler(conn, js, time);
 }
